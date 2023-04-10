e7863ea8b793af717b2c8ed1b07fba8c90200d21
EJLee1209 <dldmswo1209@gmail.com>
2023-04-10 13:38:51 +0900

    Initial Commit

---
+++ SwiftUI_Todo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI_Todo
+//
+//  Created by 이은재 on 2023/04/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Todo/SwiftUI_TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TodoApp.swift
+//  SwiftUI_Todo
+//
+//  Created by 이은재 on 2023/04/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
